82f6fd1772f21b65dc32d34afb95e9ffeb637772
igorgaev2014 <igorgaev2014@gmail.com>
2022-01-27 13:50:05 +0300

    2.0 Дорисовал гамбургер Реализация меню с JS Реализация меню без JS Валидация для номера телефона Вернул outline кнопкам для доступности

--- build/js/app.js
+++ build/js/app.js
@@ -2,6 +2,10 @@
 var navList = document.querySelector('.main-nav__list');
 var navButton = document.querySelector('.main-nav__button');
 var navToggle = document.querySelector('.main-nav__toggle');
+var navList = document.querySelector('.main-nav__list');
+
+navButton.classList.remove('main-nav__button--nojs');
+navList.classList.remove('main-nav__list--nojs');
 
 navButton.addEventListener('click', function () {
   if (navList.classList.contains('main-nav__list--closed')) {
@@ -10,11 +14,17 @@ navButton.addEventListener('click', function () {
 
     navToggle.classList.remove('main-nav__toggle--closed');
     navToggle.classList.add('main-nav__toggle--opened');
+
+    navButton.classList.remove('main-nav__button--closed');
+    navButton.classList.add('main-nav__button--opened');
   } else {
     navList.classList.add('main-nav__list--closed');
     navList.classList.remove('main-nav__list--opened');
 
     navToggle.classList.add('main-nav__toggle--closed');
     navToggle.classList.remove('main-nav__toggle--opened');
+
+    navButton.classList.add('main-nav__button--closed');
+    navButton.classList.remove('main-nav__button--opened');
   }
 });
